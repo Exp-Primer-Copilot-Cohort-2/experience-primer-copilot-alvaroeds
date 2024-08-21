3bae9e8822ff02cfa63b233ef9137f2b08aabfed
Alvaro Enrique <alvaroenrique020@gmail.com>
2024-08-21 03:29:47 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+  return {
+    skills: [],
+    addSkill: function(skill) {
+      this.skills.push(skill);
+    },
+    removeSkill: function(skill) {
+      this.skills = this.skills.filter((s) => s !== skill);
+    },
+    getSkills: function() {
+      return this.skills;
+    },
+  };
+}
